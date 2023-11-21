fa213615ddb250a1f3f25ab10e30f5b10207422e
Jack Hogan <jackhogan11@gmail.com>
2023-11-20 23:12:12 -0500

    Added autopairs

---
+++ lua/plugins/nvim_autopairs.lua
@@ -0,0 +1,16 @@
+-- No example configuration was found for this plugin.
+--
+-- For detailed information on configuring this plugin, please refer to its
+-- official documentation:
+--
+--   https://github.com/windwp/nvim-autopairs
+--
+-- If you wish to use this plugin, you can optionally modify and then uncomment
+-- the configuration below.
+
+return {
+  -- "windwp/nvim-autopairs"
+  'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
+}
